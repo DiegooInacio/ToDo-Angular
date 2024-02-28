b51181bf8ad80ab4ee8cf7a73579592acad953ff
Diego Inácio Nogueira <diego.inacio07@aluno.ifce.edu.br>
2024-02-28 16:28:12 -0300

    atualizando side-bar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TarefasComponent } from './tarefas/tarefas.component';
+import { ListasComponent } from './listas/listas.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'tarefas',
+    pathMatch: 'full',
+  },
+  {
+    path: 'tarefas',
+    component: TarefasComponent,
+  },
+  {
+    path: 'listas',
+    component: ListasComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
