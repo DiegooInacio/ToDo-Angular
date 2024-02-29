3a5d780df76ea88a99cb584eece630f75eb134b4
Diego Inácio Nogueira <diego.inacio07@aluno.ifce.edu.br>
2024-02-29 16:59:02 -0300

    atualizando side-bar 2.0

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,18 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'ToDo';
+
+  mostrarMenu: boolean = true;
+
+  ativarMenu() {
+    if (this.mostrarMenu == true) {
+      this.mostrarMenu = false;
+    } else {
+      this.mostrarMenu = true;
+    }
+  }
 }
